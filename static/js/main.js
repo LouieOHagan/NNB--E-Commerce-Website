d602c7f8e74b4a2b0132fbc25f93c136eda94b45
Louie O Hagan <louieohagan101@gmail.com>
2020-05-10 17:35:02 +0000

    Maint: Adjusted styling for navigation bar and made it fully responsive with media queries. Also put html in html formatter to beautify the code along with ensuring all js/css links are correctly commented and added some new navigation items to dropdown aswell as hooking up some of the hrefs to working urls

--- static/js/main.js
+++ static/js/main.js
@@ -1,15 +1,17 @@
 $(document).ready(function () {
   $("#search-btn").click(function () {
     $(".center-items").addClass("center-items-hidden");
+    $(".center-list").addClass("center-items-centered");
     $(".search-form").addClass("show-form");
     $(".close-search").addClass("active");
-    $(".right-list").hide();
+    $(".right-list").addClass("hide-icons");
   });
 
   $(".close-search").click(function () {
     $(".center-items").removeClass("center-items-hidden");
+    $(".center-list").removeClass("center-items-centered");
     $(".search-form").removeClass("show-form");
     $(".close-search").removeClass("active");
-    $(".right-list").show();
+    $(".right-list").removeClass("hide-icons");
   });
 });
