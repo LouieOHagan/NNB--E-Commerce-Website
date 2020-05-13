edb111f6cf3885c6107fe30281eeaecac71dfef1
Louie O Hagan <louieohagan101@gmail.com>
2020-05-13 18:45:57 +0000

    Feat: added sort by drop down button to sort products in order, with the choice of Price or Name in ascending or descending order. Got buttons working correctly and used template variables to ensure user stays on category they were in when wanting to sort the products. Also for Defensive programming added a comma after the variable incase a user manually inputs a second product type in the url to sort the 2+ types at the same time

--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.shortcuts import render
 from django.db.models import Q
 
 from .models import Product, ProductType
@@ -8,12 +8,24 @@ def display_products(request):
     all_products = Product.objects.all()
     product_type = None
     query = None
+    sort_by = None
+    direction = None
 
     if request.GET:
         if 'product_type' in request.GET:
             product_type = request.GET['product_type'].split(',')
             all_products = all_products.filter(category__name__in=product_type)
             product_type = ProductType.objects.filter(name__in=product_type)
+
+        if 'sort' in request.GET:
+            sort_by = request.GET['sort']
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == "desc":
+                    sort_by = f'-{sort_by}'
+
+            all_products = all_products.order_by(sort_by)
 
         if 'q' in request.GET:
             query = request.GET['q']
@@ -23,10 +35,13 @@ def display_products(request):
 
             all_products = all_products.filter(query_results)
 
+    current_sorting = f'{sort_by}_{direction}'
+
     context = {
         'products': all_products,
         'product_type': product_type,
         'search_content': query,
+        'sorted_by': current_sorting
     }
 
     return render(request, "products/products-page.html", context)
