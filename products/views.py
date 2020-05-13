e241e77ff36ff938f5d2d3e99dfb1247f49507ae
Louie O Hagan <louieohagan101@gmail.com>
2020-05-13 12:57:14 +0000

    SFeat: setup filtering products by product types so that the links in the navigation bar work correctly and only display the product  type that the user is requesting. Also updated all links to work in base.html and confirmed data in showing and filtering is working in products-page.html

--- products/views.py
+++ products/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 
-from .models import Product
+from .models import Product, ProductType
 
 
 def display_products(request):
-    products = Product.objects.all()
+    all_products = Product.objects.all()
+    product_type = None
+
+    if request.GET:
+        if 'product_type' in request.GET:
+            product_type = request.GET['product_type'].split(',')
+            all_products = all_products.filter(category__name__in=product_type)
+            product_type = ProductType.objects.filter(name__in=product_type)
 
     context = {
-        'products': products,
+        'products': all_products,
+        'product_type': product_type,
     }
 
     return render(request, "products/products-page.html", context)
