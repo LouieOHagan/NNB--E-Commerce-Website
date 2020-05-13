f62edd5cc8562099555d9aba3fe6585528305d59
Louie O Hagan <louieohagan101@gmail.com>
2020-05-13 14:38:28 +0000

    Feat: Added search functionality in display_products view inside of products app and linked search bar in base.html up with the view. Also adjusted the submit button for mobile navigation menu as the button once submitting the form, fixed by adding button element around the i element.

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.db.models import Q
 
 from .models import Product, ProductType
 
@@ -6,6 +7,7 @@ from .models import Product, ProductType
 def display_products(request):
     all_products = Product.objects.all()
     product_type = None
+    query = None
 
     if request.GET:
         if 'product_type' in request.GET:
@@ -13,9 +15,18 @@ def display_products(request):
             all_products = all_products.filter(category__name__in=product_type)
             product_type = ProductType.objects.filter(name__in=product_type)
 
+        if 'q' in request.GET:
+            query = request.GET['q']
+            query_results = Q(name__icontains=query) | \
+                Q(product_code__exact=query) | \
+                Q(product_description__icontains=query)
+
+            all_products = all_products.filter(query_results)
+
     context = {
         'products': all_products,
         'product_type': product_type,
+        'search_content': query,
     }
 
     return render(request, "products/products-page.html", context)
