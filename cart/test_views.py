61653f9e45cca1c100c17161262bc2720af26947
Louie O Hagan <louieohagan101@gmail.com>
2020-05-16 19:20:10 +0000

    Testing: Started tests for cart app views in test_views.py, successfully conducted tests for two of the four views which resulted in 77% coverage for the cart app.

---
+++ cart/test_views.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from products.models import Product
+
+
+class TestViews(TestCase):
+
+    def test_view_cart(self):
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'cart/cart.html')
+
+    def test_add_to_cart(self):
+        product = Product.objects.create(name='Test Product',
+                                         product_code='1234',
+                                         product_description='Test',
+                                         price_current=123)
+        response = self.client.post(f'/cart/add/{product.id}/', {'quantity': 1, 'redirect_url': f'/products/{product.id}/'})
+        self.assertRedirects(response, f'/products/{product.id}/')
+
+    # def test_update_cart(self):
+    #     product = Product.objects.create(name='Test Product',
+    #                                      product_code='1234',
+    #                                      product_description='Test',
+    #                                      price_current=123)
+    #     response = self.client.post(f'/cart/update/{product.id}/', {'quantity': 1,})
+    #     self.assertRedirects(response, f'/cart/')
+
+    # def test_remove_item_from_cart(self):
+    #     product = Product.objects.create(name='Test Product',
+    #                                      product_code='1234',
+    #                                      product_description='Test',
+    #                                      price_current=123)
+    #     response = self.client.get(f'/cart/remove/{product.id}/')
+    #     self.assertEqual(self.client.session.pop(f'{product.id}'))
+    #     self.assertRedirects(response, f'/cart/')
