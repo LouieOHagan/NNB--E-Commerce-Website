06486ff8d5b815d7a90224e4e56f947127300c5c
Louie O Hagan <louieohagan101@gmail.com>
2020-05-16 23:05:14 +0000

    Feat: installed crispy forms, froze it to requirements.txt and setup in settings.py. Also setup main checkout view in views.py within checkout app and created & got crispy form displaying on checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,9 +1,18 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render, redirect, reverse
+from .forms import OrderForm
 
 
 def checkout(request):
     """ view_cart view renders the cart page.
     It displays the items in the users cart
     """
+    cart = request.session.get('cart', {})
+    if not cart:
+        return redirect(reverse('display_products'))
 
-    return HttpResponse("This is our new page")
+    order_form = OrderForm()
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, 'checkout/checkout.html', context)
--- nnb_store/settings.py
+++ nnb_store/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
     # Other
     'storages',
+    'crispy_forms',
 ]
 
 SITE_ID = 1
@@ -60,6 +61,8 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'nnb_store.urls'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 TEMPLATES = [
     {
@@ -78,6 +81,10 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'cart.contexts.cart_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
