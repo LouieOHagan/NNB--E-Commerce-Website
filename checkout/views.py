e05c91ba857c8f1a7fdc66b7dec1d1e5fdf71b40
Louie O Hagan <louieohagan101@gmail.com>
2020-05-17 12:52:25 +0000

    Feat: setup basic checkout functionality using stripe so that form is only submitted through javascript if there are no stripe errors & also added stripe keys to views.py within checkout app and added functionality for stripe to get the carts grand total which is rounded and converted to an integer which stripe requires

--- checkout/views.py
+++ checkout/views.py
@@ -1,20 +1,41 @@
 from django.shortcuts import render, redirect, reverse
+from django.conf import settings
+from django.contrib import messages
+
 from .forms import OrderForm
+from cart.contexts import cart_contents
+import stripe
 
 
 def checkout(request):
     """ view_cart view renders the cart page.
     It displays the items in the users cart
     """
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         return redirect(reverse('display_products'))
 
+    current_cart = cart_contents(request)
+    total = current_cart['grand_total']
+    stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it as an enviroment variable')
+
     order_form = OrderForm()
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_u2w66BxfTEBnuXeg9ZErRRe100X4KLLGku',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, 'checkout/checkout.html', context)
