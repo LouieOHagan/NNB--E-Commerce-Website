97952ecd1de5d9f354ae8fbdc8f53f6a4cda4b08
Louie O Hagan <louieohagan101@gmail.com>
2020-05-17 15:44:55 +0000

    Feat: added webhook secret key in settings.py  & setup webhook url in urls.py within checkout app. Also added webhook for handling intenent payment_failed events in webhook_handler.py and finally created webhooks.py which contains the webhook view that is linked to the webhook url.

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -7,7 +7,21 @@ class StripeWH_Handler:
         self.request = request
 
     def handle_event(self, event):
+        """ Function handles generic/unknown webhook events """
+        return HttpResponse(
+            content=f'Unhandled Webhook recieved: {event["type"]}',
+            status=200
+        )
 
+    def handle_payment_intent_succeeded(self, event):
+        """ Function handles stripe payment_intenent.succeeded events """
+        return HttpResponse(
+            content=f'Webhook recieved: {event["type"]}',
+            status=200
+        )
+
+    def handle_payment_intent_payment_failed(self, event):
+        """ Function handles stripe payment_intenent.payment_failed events """
         return HttpResponse(
             content=f'Webhook recieved: {event["type"]}',
             status=200
--- nnb_store/settings.py
+++ nnb_store/settings.py
@@ -198,4 +198,4 @@ if 'USE_AWS' in os.environ:
 STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-# STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
