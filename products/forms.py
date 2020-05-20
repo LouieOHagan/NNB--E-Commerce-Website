e4780efc080fe803fe8c52e6a6c579364d7b2581
Louie O Hagan <louieohagan101@gmail.com>
2020-05-20 20:34:45 +0000

    Feat & Style: adjusted html & styling on form in edit-review template within products app and also adjusted the forms.py to autofocus on the first input field when the page loads and to give all input fields specific classes dynamically rather than hard coding the form fields

--- products/forms.py
+++ products/forms.py
@@ -12,3 +12,11 @@ class ReviewForm(forms.ModelForm):
             'title',
             'product_review',
         )
+
+    def __init__(self, *args, **kwargs):
+        """ Add form input classes and set autofocus on first field """
+        super().__init__(*args, **kwargs)
+
+        self.fields['display_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control review-form-item'
