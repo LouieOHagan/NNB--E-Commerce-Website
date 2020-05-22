4506953fc44ce299cee24695cd57c4f9dfb100a0
Louie O Hagan <louieohagan101@gmail.com>
2020-05-22 11:34:05 +0000

    Feat: setup toats inside of templates/includes folder using bootstraps toasts with top colour header from boutique ado project. Setup toasts js and toasts levels inside of the base.html and added messages_storages variable to settings.py for messages to work correctly. Lastly added a test message to the add to cart view which pops up a toast w/a custom message after adding an item to cart

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+
+from products.models import Product
 
 
 def view_cart(request):
@@ -15,6 +18,7 @@ def add_to_cart(request, item_id):
     ensures they are redirected to that items page
     """
 
+    product = Product.objects.get(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
 
@@ -24,6 +28,7 @@ def add_to_cart(request, item_id):
         cart[item_id] += quantity
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Added {product.name} to your cart!')
 
     request.session['cart'] = cart
 
