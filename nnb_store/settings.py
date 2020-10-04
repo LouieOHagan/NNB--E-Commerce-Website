76b54de3e8f9ef1cc52681bd6ac9e4950ecb3aea
Louie O Hagan <louieohagan101@gmail.com>
2020-10-04 11:56:57 +0100

    Maint: adjusted styling for carousel buttons and added basic idea layout for cards on home page. Also adjusted navbar width to 93% so that everything is pushed to each side and has cleaner look. Lastly removed duplicate email settings in settings.py

--- nnb_store/settings.py
+++ nnb_store/settings.py
@@ -211,15 +211,3 @@ if 'USE_AWS' in os.environ:
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'nnb.noreply@gmail.com'
-
-if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'nnb@example.com'
-else:
-    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-    EMAIL_USE_TLS = True
-    EMAIL_PORT = 587
-    EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
