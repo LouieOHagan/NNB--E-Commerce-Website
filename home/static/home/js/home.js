3d1646b573fb8b0a35b70a658d670be44020adfe
Louie O Hagan <louieohagan101@gmail.com>
2021-01-08 16:45:07 +0000

    Styling & Maint: added new CTA button to product type ads on home page and also completed styling for the text & buttons within the ads. Lastly made adjustments/cleaned code in home.js by condensing/handling everything in 1 function rather than 2

--- home/static/home/js/home.js
+++ home/static/home/js/home.js
@@ -1,18 +1,16 @@
 // Mouse Enter/Leave Event Listeners for Ad One
-document.querySelector('.ad-one').addEventListener("mouseover", imageHover.bind(this, "one"));
-document.querySelector('.ad-one').addEventListener("mouseleave", imageLeave.bind(this, "one"));
+document.querySelector('.ad-one').addEventListener("mouseover", imageHover.bind(this, "one", "enter"));
+document.querySelector('.ad-one').addEventListener("mouseleave", imageHover.bind(this, "one", "leave"));
 // Mouse Enter/Leave Event Listeners for Ad Two
-document.querySelector('.ad-two').addEventListener("mouseover", imageHover.bind(this, "two"));
-document.querySelector('.ad-two').addEventListener("mouseleave", imageLeave.bind(this, "two"));
+document.querySelector('.ad-two').addEventListener("mouseover", imageHover.bind(this, "two", "enter"));
+document.querySelector('.ad-two').addEventListener("mouseleave", imageHover.bind(this, "two", "leave"));
 
-// Adds ad-image-hover class to respective card image when mouse is hovered over card.
-function imageHover(adNumber) {
+// Adds OR removes ad-image-hover class to respective card image depending on user actions.
+function imageHover(adNumber, action) {
     let image = document.querySelector(`.ad-${adNumber}-image`);
-    image.classList.add("ad-image-hover");
-}
-
-// Removes ad-image-hover class to respective card image when mouse leaves/is no longer hovering over card.
-function imageLeave(adNumber) {
-    let image = document.querySelector(`.ad-${adNumber}-image`);
-    image.classList.remove("ad-image-hover");
+    if (action === "enter") {
+        image.classList.add("ad-image-hover");
+    } else if (action === "leave") {
+        image.classList.remove("ad-image-hover");
+    }
 }
